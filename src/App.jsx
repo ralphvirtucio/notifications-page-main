c76f2246857eb614388b5e23b3e267392e8b43e7
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-07-21 13:36:19 +0800

    Added State for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,40 @@ import { Fragment, useState } from 'react';
 import './App.scss';
 import data from './data.json';
 import { Notifications } from './components/Notifications/Notifications';
-//! Notification Badge will be a state of how many unread message on the notifications
 
 const App = () => {
   const [userData, setUserData] = useState(data);
+
+  const unreadMessage = () => {
+    const unreadMessages = userData.filter((value) => value.unread);
+
+    return unreadMessages;
+  };
+
+  const markUnreadMessage = () => {
+    console.log('click');
+
+    setUserData((prevState) =>
+      prevState.map((data) => {
+        if (data.unread) {
+          return { ...data, unread: !data.unread };
+        } else {
+          return data;
+        }
+      })
+    );
+  };
 
   return (
     <Fragment>
       <header className='header'>
         <div className='main-heading__container'>
-          <h1>Notifications</h1> <span className='notification__badge'>3</span>
+          <h1>Notifications</h1>{' '}
+          <span className='notification__badge'>{unreadMessage().length}</span>
         </div>
 
         <div className='mark-all-read__container'>
-          <button>Mark All as read</button>
+          <button onClick={markUnreadMessage}>Mark All as read</button>
         </div>
       </header>
 
